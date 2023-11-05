4a04c9fd92c150746c333cd15c931d094f92a49a
amaalyy <amal.elzowawi@gmail.com>
2023-11-06 00:06:00 +0100

    change the comments

--- models/base_model.py
+++ models/base_model.py
@@ -5,26 +5,28 @@ import datetime
 
 
 class BaseModel():
-    """BaseModel class"""
+    """BaseModel class the parent of other classes"""
 
     def __init__(self):
-        """public instance attributes"""
+        """initializing the public instance attributes"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
     # __str__ method
 
     def __str__(self):
-        """__str__ method"""
+        """method that return string representation of an instance"""
         return f"[{BaseModel.__name__}] ({self.id}) {self.__dict__}"
     # save method
 
     def save(self):
-        """save method"""
+        """method that updates the public instance attribute
+        updated_at with the current datetime"""
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
-        """to_dict"""
+        """method that returns a dictionary containing all
+          keys/values of __dict__ of the instance"""
         dic = {}
         for key, value in (self.__dict__.items()):
             if (isinstance(value, datetime.datetime)):
