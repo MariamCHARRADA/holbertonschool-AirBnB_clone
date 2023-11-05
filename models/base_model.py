fc5ef67003bbe5941bf6cb513e1abe57d48268e9
amaalyy <amal.elzowawi@gmail.com>
2023-11-05 23:49:25 +0100

    base model

---
+++ models/base_model.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Console Base module"""
+import uuid
+import datetime
+
+
+class BaseModel():
+    """BaseModel class"""
+
+    def __init__(self):
+        """public instance attributes"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+    # __str__ method
+    def __str__(self):
+        """__str__ method"""
+        return f"[{BaseModel.__name__}] ({self.id}) {self.__dict__}"
+    # save method
+    def save(self):
+        """save method"""
+        self.updated_at = datetime.datetime.now()
+    def to_dict(self):
+        """to_dict"""
+        dic = {}
+        for key, value in (self.__dict__.items()):
+            if (isinstance(value, datetime.datetime)):
+                value = value.isoformat()
+            dict[key] = value
+        dic['__class__'] = self.__class__.__name__
+        return dic
+        
+            
