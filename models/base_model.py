26c68e3d901bd78a1c1ea0b96c12afef6ebbc62a
MariamCHARRADA <charradamariam@yahoo.com>
2023-11-06 17:45:07 +0100

    format document

--- models/base_model.py
+++ models/base_model.py
@@ -4,23 +4,26 @@ import uuid
 import datetime
 
 
-class BaseModel():
+class BaseModel:
     """BaseModel class the parent of other classes"""
 
     def __init__(self, *args, **kwargs):
         """initializing the public instance attributes"""
-        if (kwargs):
-            for key, value in (kwargs.items()):
+        if kwargs:
+            for key, value in kwargs.items():
                 if key != "__class__":
                     if key == "created_at" or key == "updated_at":
                         value = datetime.datetime.strptime(
-                            value, "%Y-%m-%dT%H:%M:%S.%f")
+                            value, "%Y-%m-%dT%H:%M:%S.%f"
+                        )
                     self.to_dict[key] = value
+
     # __str__ method
 
     def __str__(self):
         """method that return string representation of an instance"""
         return f"[{BaseModel.__name__}] ({self.id}) {self.__dict__}"
+
     # save method
 
     def save(self):
@@ -30,11 +33,11 @@ class BaseModel():
 
     def to_dict(self):
         """method that returns a dictionary containing all
-          keys/values of __dict__ of the instance"""
+        keys/values of __dict__ of the instance"""
         dic = {}
-        for key, value in (self.__dict__.items()):
-            if (isinstance(value, datetime.datetime)):
+        for key, value in self.__dict__.items():
+            if isinstance(value, datetime.datetime):
                 value = value.isoformat()
             dict[key] = value
-        dic['__class__'] = self.__class__.__name__
+        dic["__class__"] = self.__class__.__name__
         return dic
