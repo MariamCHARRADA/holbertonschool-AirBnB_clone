ea725132c7aaa75fbd54865a6faff9d51b4e6d2f
MariamCHARRADA <charradamariam@yahoo.com>
2023-11-06 23:36:32 +0100

    Basemodel unittesting

---
+++ tests/test_base_model.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Basemodel unittesting"""
+import unittest
+from models.base_model import BaseModel
+import datetime
+
+
+class TestBaseModel(unittest.TestCase):
+    def test_instance_creation(self):
+        instance = BaseModel()
+        self.assertIsInstance(instance, BaseModel)
+        self.assertIsInstance(instance.id, str)
+        self.assertIsInstance(instance.created_at, datetime.datetime)
+        self.assertIsInstance(instance.updated_at, datetime.datetime)
+
+    def test_str_representation(self):
+        instance = BaseModel()
+        str_repr = str(instance)
+        self.assertIn("[BaseModel]", str_repr)
+        self.assertIn(str(instance.id), str_repr)
+        self.assertIn(str(instance.__dict__), str_repr)
+
+    def test_save_method(self):
+        instance = BaseModel()
+        original_updated_at = instance.updated_at
+        instance.save()
+        self.assertNotEqual(original_updated_at, instance.updated_at)
+
+    def test_to_dict_method(self):
+        instance = BaseModel()
+        instance_dict = instance.to_dict()
+        self.assertIsInstance(instance_dict, dict)
+        self.assertIn("__class__", instance_dict)
+        self.assertEqual(instance_dict["__class__"], "BaseModel")
+
+
+if __name__ == "__main__":
+    unittest.main()
