2c840d1035696769fb2c17d3967a9f2869505cb6
amaalyy <amal.elzowawi@gmail.com>
2023-11-07 12:40:18 +0100

    fix pycodestyle

--- models/review.py
+++ models/review.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """Class of Review"""
 from models.base_model import BaseModel
+
+
 class Review(BaseModel):
     """Review Class that inherit from BAseModel Class"""
     place_id = ""
     user_id = ""
     text = ""
+
     def __str__(self):
         """method that return string repesentation of an instance"""
         return f"[{Review.__name__}] ({self.id}) {self.__dict__}"
