75814e31c0ef1d8a404f24cbbc0e984077f0da6e
amaalyy <amal.elzowawi@gmail.com>
2023-11-07 11:59:48 +0100

    fix pycodestyle

--- models/state.py
+++ models/state.py
@@ -6,6 +6,7 @@ from models.base_model import BaseModel
 class state(BaseModel):
     """State Class that inherit from BaseModel Class"""
     name = ""
+
     def __str__(self):
         """method that return string repesentation of an instance"""
-        return f"[{state.__name__}] ({self.id}) {self.__dict__}" 
+        return f"[{state.__name__}] ({self.id}) {self.__dict__}"
