55740a1f4f4327de24f71f529905c8e203a3bb4c
amaalyy <amal.elzowawi@gmail.com>
2023-11-07 11:55:06 +0100

    Class state

---
+++ models/state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Class of State"""
+from models.base_model import BaseModel
+
+
+class state(BaseModel):
+    """State Class that inherit from BaseModel Class"""
+    name = ""
+    def __str__(self):
+        """method that return string repesentation of an instance"""
+        return f"[{state.__name__}] ({self.id}) {self.__dict__}" 
