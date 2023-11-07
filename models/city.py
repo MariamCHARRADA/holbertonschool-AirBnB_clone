bc9242ee7dc9c610352755e609246821bd12bf25
amaalyy <amal.elzowawi@gmail.com>
2023-11-07 12:17:03 +0100

    fix pycodestyle

--- models/city.py
+++ models/city.py
@@ -3,11 +3,11 @@
 from models.base_model import BaseModel
 
 
-class city(BaseModel):
+class City(BaseModel):
     """City Class that inherit from BaseModel Class"""
     state_id = ""
     name = ""
 
     def __str__(self):
         """method that return string repersentation of an instance"""
-        return f"[{city.__name__}] ({self.id}) {self.__dict__}"
+        return f"[{City.__name__}] ({self.id}) {self.__dict__}"
