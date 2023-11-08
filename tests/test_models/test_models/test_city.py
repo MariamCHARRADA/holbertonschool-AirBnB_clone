b94b20da43056397a9bb8cd1c26c0a5776028c7b
amaalyy <amal.elzowawi@gmail.com>
2023-11-08 12:50:01 +0100

    test file

---
+++ tests/test_models/test_models/test_city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""unittest for the City"""
+
+from models.base_model import BaseModel
+from models.city import City
+import unittest
+import datetime
+
+class Test_City(unittest.TestCase):
+    """Class Test for City"""
+    def test_state_id(self):
+        self.assertEqual(str, type(City.state_id))
+    def test_name(self):
+        self.assertEqual(str, type(City.name))
