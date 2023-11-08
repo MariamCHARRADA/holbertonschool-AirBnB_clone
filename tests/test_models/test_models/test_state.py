f9e4355b79b14cb36ceb74cc84271731f0ebad93
amaalyy <amal.elzowawi@gmail.com>
2023-11-08 12:31:12 +0100

    fix

--- tests/test_models/test_models/test_state.py
+++ tests/test_models/test_models/test_state.py
@@ -6,7 +6,9 @@ from models.state import State
 import unittest
 import datetime
 
+
 class Test_State(unittest.TestCase):
     """Class Test for State"""
+
     def test_name_is_public_str(self):
         self.assertEqual(str, type(State.name))
