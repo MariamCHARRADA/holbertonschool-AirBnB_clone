4274cebdaf7b734cdac3bed8e29ee6b1cda53d84
MariamCHARRADA <charradamariam@yahoo.com>
2023-11-08 03:13:46 +0100

    console initialization

---
+++ console.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """Class HBNBCommand that inherits from cmd.Cmd"""
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        return True
+    
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
