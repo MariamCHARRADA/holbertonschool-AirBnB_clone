583a6c4fa9914c63a28a880ea1724d64447c12fa
amaalyy <amal.elzowawi@gmail.com>
2023-11-08 12:30:43 +0100

    test file

---
+++ tests/test_models/test_models/test_state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""unittest for State"""
+
+from models.base_model import BaseModel
+from models.state import State
+import unittest
+import datetime
+
+class Test_State(unittest.TestCase):
+    """Class Test for State"""
+    def test_name_is_public_str(self):
+        self.assertEqual(str, type(State.name))
