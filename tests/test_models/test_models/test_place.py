f23dad90594468c86cdf7c0edb5004cc5fae285c
amaalyy <amal.elzowawi@gmail.com>
2023-11-08 13:24:46 +0100

    fix pycodestyle

--- tests/test_models/test_models/test_place.py
+++ tests/test_models/test_models/test_place.py
@@ -6,15 +6,21 @@ from datetime import datetime
 import unittest
 import uuid
 
+
 class Test_Place(unittest.TestCase):
     """Class Test for Place"""
+
     def test_city_id(self):
         self.assertEqual(str, type(Place.city_id))
+
     def test_name(self):
         self.assertEqual(str, type(Place.name))
+
     def test_number_rooms(self):
         self.assertEqual(str, type(Place.number_rooms))
+
     def test_latitude(self):
         self.assertEqual(float, type(Place.latitude))
+
     def test_amenity_ids(self):
         self.assertEqual(list, type(Place.amenity_ids))
