ddd5d654d00904340564a65b8c405b9ffe458273
MariamCHARRADA <charradamariam@yahoo.com>
2023-11-12 10:48:36 +0100

    update user module docstrings

--- models/user.py
+++ models/user.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""Class if User"""
+"""User module"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """User Class that inherits from BaseMidel"""
+    """User Class that inherits from BaseModel"""
+
     email = ""
     password = ""
     first_name = ""
     last_name = ""
 
     def __str__(self):
-        """method that return string repesentation of an instance"""
+        """method that returns the string representation of an instance"""
         return f"[{User.__name__}] ({self.id}) {self.__dict__}"
