2ccf179b4925fea3d2f3868695d0f016b0936a73
amaalyy <amal.elzowawi@gmail.com>
2023-11-13 13:35:12 +0100

    unittest for amenity

---
+++ tests/test_models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""unittest for the Amenity"""
+from models.amenity import Amenity
+from datetime import datetime
+import unittest
+import uuid
+
+class Test_Amenity(unittest.TestCase):
+    """Class test for Amenity"""
+    def test_name_is_public_str(self):
+        self.assertEqual(str, type(Amenity.name))
