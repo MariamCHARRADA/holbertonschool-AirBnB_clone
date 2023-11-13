51c9d00c97c983093c5156ada9edb0599c74dea5
amaalyy <amal.elzowawi@gmail.com>
2023-11-13 13:36:33 +0100

    fix error

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -6,7 +6,7 @@ from datetime import datetime
 import uuid
 
 
-class Test_User(unittest.TestCase):
+class Test_State(unittest.TestCase):
     """Class Test for State"""
 
     def test_name_is_public_str(self):
